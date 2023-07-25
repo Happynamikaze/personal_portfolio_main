63a4cc54f6dd0eb79f38ebd2e7f76a9c6ecbeeb5
Sarabjeet Singh <88402524+Happynamikaze@users.noreply.github.com>
2023-07-26 01:10:59 +0530

    some new page done portfolio

--- js/mail.js
+++ js/mail.js
@@ -1,28 +1,28 @@
-function sendEmail() {
+// function sendEmail() {
 
-    let name = $("#name").val();
-    let subject = $("#subject").val();
-    let email = $("#email").val();
-    let message = $("#message").val();
+//     let name = $("#name").val();
+//     let subject = $("#subject").val();
+//     let email = $("#email").val();
+//     let message = $("#message").val();
 
-    let mainBody = "Name: " + name + "<br>Subject: " + subject + "<br>Email: " + email + "<br>Message: " + message;
-}
-Email.send({
-    Host : "#",
-    Username : "#",
-    Password : "#",
-    To : '#',
-    From : '#',
-    Subject : document.querySelector('#subject').value,
-    Body : mainBody
-}).then(
-  message => {
-      if(message == 'OK') {
-          $('.mailSent').addClass('show');
-          $('.mailSent').removeClass('d-none');
-      } else {
-        $('.mailErr').addClass('show');
-        $('.mailErr').removeClass('d-none');
-      }
-  }
-);
+//     let mainBody = "Name: " + name + "<br>Subject: " + subject + "<br>Email: " + email + "<br>Message: " + message;
+// }
+// Email.send({
+//     Host : "#",
+//     Username : "#",
+//     Password : "#",
+//     To : '#',
+//     From : '#',
+//     Subject : document.querySelector('#subject').value,
+//     Body : mainBody
+// }).then(
+//   message => {
+//       if(message == 'OK') {
+//           $('.mailSent').addClass('show');
+//           $('.mailSent').removeClass('d-none');
+//       } else {
+//         $('.mailErr').addClass('show');
+//         $('.mailErr').removeClass('d-none');
+//       }
+//   }
+// );
--- js/script.js
+++ js/script.js
@@ -70,4 +70,19 @@ options = {
 //         // fadeOut complete. Remove the loading div
 //         $( "#after-loder-show" ).remove(); //makes page more lightweight 
 //     });  
-//   }
+//   }const videos = document.querySelectorAll("video")
+
+
+
+// Listening to the video element
+let clip = document.querySelector(".vid")
+
+/* Adding the event listeners on the video to play/pause the video. */
+clip.addEventListener("mouseover", function (e) {
+   clip.play();
+})
+
+/* Applying the mouse out event to pause the video */
+clip.addEventListener("mouseout", function (e) {
+   clip.pause();
+})
